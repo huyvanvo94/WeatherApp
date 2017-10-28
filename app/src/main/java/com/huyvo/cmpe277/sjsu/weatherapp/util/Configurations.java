64b9efa210e21e07e8ed348ec4bb1caf1a9787dd
huyvo <huyvanvo94@gmail.com>
2017-10-28 10:29:10 -0700

    update settings

--- app/src/main/java/com/huyvo/cmpe277/sjsu/weatherapp/util/Configurations.java
+++ app/src/main/java/com/huyvo/cmpe277/sjsu/weatherapp/util/Configurations.java
@@ -7,11 +7,23 @@ package com.huyvo.cmpe277.sjsu.weatherapp.util;
 public class Configurations {
     public static boolean isImperial = true;
 
-    public static String getUnit(){
+    public synchronized static String getUnit(){
         if(isImperial){
             return "imperial";
         }
 
         return "metric";
     }
+
+    public synchronized static String getWeatherRestUrl(String location){
+        return "http://api.openweathermap.org/data/2.5/weather?"
+                +location+"&units="+getUnit()+"&appid=b54f500d4a53fdfc96813a4ba9210417";
+    }
+
+    public synchronized static String getForcastRestUrl(String location){
+        return "http://api.openweathermap.org/data/2.5/forecast/daily?"
+                +location+"&mode=json&units="+getUnit()+"&cnt=7&appid=b54f500d4a53fdfc96813a4ba9210417";
+    }
+
+
 }
