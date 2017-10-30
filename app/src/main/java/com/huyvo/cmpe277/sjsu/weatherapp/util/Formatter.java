3de809b7d1fde152df97953dc2d7664e03ca99a3
kevin-gadek <kgadek@sbcglobal.net>
2017-10-29 18:46:15 -0700

    update

--- app/src/main/java/com/huyvo/cmpe277/sjsu/weatherapp/util/Formatter.java
+++ app/src/main/java/com/huyvo/cmpe277/sjsu/weatherapp/util/Formatter.java
@@ -9,9 +9,41 @@ import com.huyvo.cmpe277.sjsu.weatherapp.model.WeatherModel;
 public class Formatter {
     public String formatTemperature(float temp){
         if(Configurations.isImperial){
-            return String.valueOf(temp) + "\u00B0" + " F";
+            return String.valueOf((int)temp) + "\u00B0" + " F";
         }else{
-            return String.valueOf(temp) + "\u00B0" + " C";
+            return String.valueOf((int)temp) + "\u00B0" + " C";
+        }
+    }
+
+    public String formatMaxTemperature(float temp_max){
+        if(Configurations.isImperial){
+            return "Max: " + String.valueOf((int)temp_max) + "\u00B0" + " F";
+        }else{
+            return "Max: " + String.valueOf((int)temp_max) + "\u00B0" + " C";
+        }
+    }
+
+    public String formatMinTemperature(float temp_min){
+        if(Configurations.isImperial){
+            return "Min: " + String.valueOf((int)temp_min) + "\u00B0" + " F";
+        }else{
+            return "Min: " + String.valueOf((int)temp_min) + "\u00B0" + " C";
+        }
+    }
+
+    public String formatHumidity(int humidity){
+        return String.valueOf(humidity) + " %";
+    }
+
+    public String formatPressure(int pressure){
+        return String.valueOf(pressure) + " hPa";
+    }
+
+    public String formatWindSpeed(float windSpeed){
+        if(Configurations.isImperial){
+            return String.valueOf((int) windSpeed) + " mph";
+        }else{
+            return String.valueOf((int) windSpeed) + " m/sec";
         }
     }
 
